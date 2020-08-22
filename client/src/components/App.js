9bfdc0951e86292cc7eca92e1a6f3747ab4defd9
marco-amorim <marcoamorim95@hotmail.com>
2020-08-21 21:47:43 -0300

    refactor routes in separated file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,19 +6,14 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import Routes from '../Routes';
 
 const App = () => {
 	return (
 		<div className="ui container">
 			<BrowserRouter>
-				<div>
-					<Header />
-					<Route path="/" exact component={StreamList} />
-					<Route path="/streams/new" exact component={StreamCreate} />
-					<Route path="/streams/edit" exact component={StreamEdit} />
-					<Route path="/streams/delete" exact component={StreamDelete} />
-					<Route path="/streams/show" exact component={StreamShow} />
-				</div>
+				<Header />
+				<Routes />
 			</BrowserRouter>
 		</div>
 	);
