65b52313007fbe22125034d69ef1026c98ce50d0
marco-amorim <marcoamorim95@hotmail.com>
2020-09-06 23:00:03 -0300

    routes file name

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './Header';
-import Routes from '../Routes';
+import Routes from '../routes';
 
 const App = () => {
 	return (
