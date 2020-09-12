a2cada8cd416c7f513fc6bd2f51531317990719d
marco-amorim <marcoamorim95@hotmail.com>
2020-09-12 20:41:02 -0300

    sign out and sign in buttons

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -23,19 +23,27 @@ class GoogleAuth extends Component {
 		this.setState({ isSignedIn: this.auth.isSignedIn.get() });
 	};
 
+	onSignIn = () => {
+		this.auth.signIn();
+	};
+
+	onSignOut = () => {
+		this.auth.signOut();
+	};
+
 	renderAuthButton() {
 		if (this.state.isSignedIn === null) {
 			return null;
 		} else if (this.state.isSignedIn) {
 			return (
-				<button className="ui red google button">
+				<button onClick={this.onSignOut} className="ui red google button">
 					<i className="google icon" />
 					Sign out
 				</button>
 			);
 		} else {
 			return (
-				<button className="ui red google button">
+				<button onClick={this.onSignIn} className="ui green google button">
 					<i className="google icon" />
 					Sign in with Google
 				</button>
